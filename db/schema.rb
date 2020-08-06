f13e550ba4bc15100643bd9168e03d1d3bfe5a0a
vagrant <[eugenenzv8@gmail.com]>
2020-08-06 09:08:40 +0000

    add favorite

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_080248) do
+ActiveRecord::Schema.define(version: 2020_08_06_085450) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
