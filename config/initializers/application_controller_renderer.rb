8565377fc517fb2b9cc166e9392efe8c72789292
vagrant <[eugenenzv8@gmail.com]>
2020-08-06 07:51:18 +0000

    add PostImage

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd1d0e81e0423ad8bbc7fcfc910fd93933f55233d85bca27a152d09a247116b71e2655c05e1b1f1b54d73d8bb6edff00472800ec00728155b39a8ad797130a2f4'
